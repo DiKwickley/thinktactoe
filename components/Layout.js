107a580542574cedaebd0816c47b7a4d8b487e4e
Aniket Singh Rawat <sprx077@gmail.com>
2021-07-07 14:02:35 +0530

    nav

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import Head from "next/head";
 
+import { Nav } from "./Nav";
+
 export function Layout({ children, title = "This is the default title" }) {
   return (
     <>
@@ -9,7 +11,9 @@ export function Layout({ children, title = "This is the default title" }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className="header">Nav</div>
+      <div className="header">
+        <Nav />
+      </div>
 
       <div className="main">{children}</div>
       {/* <footer>{"I`m here to stay"}</footer> */}
