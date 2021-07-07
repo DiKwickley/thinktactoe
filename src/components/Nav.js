5a843ebad6dec0d80a30dfa50b32176661838639
Aniket Singh Rawat <sprx077@gmail.com>
2021-07-08 00:22:48 +0530

    updated nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -22,23 +22,43 @@ export const Nav = () => {
   };
 
   return (
-    <div>
-      <section>
-        {user ? (
-          <div>
-            logged in as {user.displayName}
-            <button
-              onClick={() => {
-                auth.signOut();
-              }}
-            >
-              Signout
-            </button>
-          </div>
-        ) : (
-          <button onClick={signInWithGoogle}>Sign in with Google</button>
-        )}
-      </section>
+    <div className="flex flex-row justify-end">
+      {user ? (
+        // <div>
+        //   logged in as {user.displayName}
+        //   <button
+        //     className
+        //     onClick={() => {
+        //       auth.signOut();
+        //     }}
+        //   >
+        //     Signout
+        //   </button>
+        // </div>
+        <div
+          onClick={() => {
+            var res = confirm("Do you want to sign out?");
+            if (res) {
+              auth.signOut();
+            }
+          }}
+          className="flex flex-row items-center justify-center px-3 py-1 m-3 border-2 border-black rounded-lg cursor-pointer"
+        >
+          <img className="h-[50px] rounded-full  mr-2" src={user.photoURL} />
+          {user.displayName}
+        </div>
+      ) : (
+        <div
+          onClick={signInWithGoogle}
+          className="flex flex-row items-center justify-center pr-3 m-3 border-2 border-black rounded-lg cursor-pointer"
+        >
+          <img
+            className="h-[50px]"
+            src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/google/google.png"
+          />
+          Google
+        </div>
+      )}
     </div>
   );
 };
